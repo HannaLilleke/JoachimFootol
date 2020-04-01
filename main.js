0da593caa8634609673da4cea3bf5042ba1f2524
Martti Nõmme <marttinomme@gmail.com>
2020-04-01 22:53:06 +0300

    .

---
+++ main.js
@@ -0,0 +1,24 @@
+function openModal() {
+    document.getElementById('my-modal').style.display = "inline-flex";
+}
+function closeModal() {
+    document.getElementById('my-modal').style.display = "none";
+}
+function showParagraph1() {
+    document.getElementById('paragraph-container').style.display = "block";
+}
+function hideParagraph1() {
+    document.getElementById('paragraph-container').style.display = "none";
+}
+function showParagraph2() {
+    document.getElementById('paragraph2').style.display = "block";
+}
+function hideParagraph2() {
+    document.getElementById('paragraph2').style.display = "none";
+}
+function showParagraph3() {
+    document.getElementById('paragraph-div').style.display = "block";
+}
+function hideParagraph3() {
+    document.getElementById('paragraph-div').style.display = "none";
+}
